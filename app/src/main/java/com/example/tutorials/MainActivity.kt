3d7f698deb690513e3ebe1f9af07068c8536f905
Chevischevis <chevischevis@ciencias.unam.mx>
2025-12-13 21:26:28 -0600

    Probando el comportamiento del Overlapping

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
@@ -26,13 +29,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.ScaleFactor
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tutorials.ui.theme.TutorialsTheme
 import kotlinx.coroutines.launch
+import java.lang.ProcessBuilder.Redirect.to
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +51,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HorizontalExample()
+                    TestOvelappingCrayRow()
                 }
 
             }
@@ -52,66 +59,87 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun HorizontalExample(modifier: Modifier = Modifier) {
-    val colorList = listOf(Color.Red, Color.Green, Color.Blue, Color.Cyan, Color.Magenta)
-    val pageState = rememberPagerState {
-        colorList.size
-    }
-    val coroutineScope = rememberCoroutineScope()
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+fun OverlappingCrazyRow(
+    modifier: Modifier = Modifier,
+    @FloatRange(from = 0.1, to = 1.0) overlapFactor: Float = 0.5f,
+    @FloatRange(from = 0.0, to = 0.5) moveDownFactor: Float = 0f,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
 
-        HorizontalPager(state = pageState) {
-            Box(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-                    .background(colorList[it]),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Page: $it",
-                    color = Color.White,
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+    ) { measurables, constrains ->
+        val placeables = measurables.map { measurables ->
+            measurables.measure(constrains)
         }
-        Row {
-            Icon(
-                imageVector = Icons.Filled.KeyboardArrowLeft,
-                contentDescription = "Previous Page",
-                modifier = Modifier
-                    .size(75.dp)
-                    .clickable{
-                        coroutineScope.launch {
-                            pageState.animateScrollToPage(pageState.currentPage -1)
-                        }
-                    }
-
-            )
-            Icon(
-                imageVector = Icons.Filled.KeyboardArrowRight,
-                contentDescription = "Previous Page",
-                modifier = Modifier
-                    .size(75.dp)
-                    .clickable{
-                        coroutineScope.launch {
-                            pageState.animateScrollToPage(pageState.currentPage +1)
-                        }
-                    }
-            )
+        val heightMax = placeables.maxOf { it.height }
+        val moveDown = (heightMax * moveDownFactor).toInt()
+        val heigh = heightMax + moveDown
+        val width = (placeables.subList(1, placeables.size)
+            .sumOf { it.width } * overlapFactor + placeables[0].width).toInt()
+        layout(width, heigh) {
+            var xPos = 0
+            for ((index, placeable) in placeables.withIndex()) {
+                if (index % 2 == 0) {
+                    placeable.placeRelative(xPos, 0, 0f)
+                } else {
+                    placeable.placeRelative(xPos, moveDown, 0f)
+                }
+                xPos += (placeable.width * overlapFactor).toInt()
+            }
         }
     }
 }
 
+@Composable
+fun TestOvelappingCrayRow() {
+    OverlappingCrazyRow(overlapFactor = 0.7f, moveDownFactor = 0.2f) {
+
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color.White, shape = CircleShape)
+                .clip(CircleShape)
+                .background(Color.Blue)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color.White, shape = CircleShape)
+                .clip(CircleShape)
+                .background(Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color.White, shape = CircleShape)
+                .clip(CircleShape)
+                .background(Color.Cyan)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color.White, shape = CircleShape)
+                .clip(CircleShape)
+                .background(Color.Magenta)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color.White, shape = CircleShape)
+                .clip(CircleShape)
+                .background(Color.Green)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TutorialsTheme {
-        HorizontalExample()
+        TestOvelappingCrayRow()
     }
 }
