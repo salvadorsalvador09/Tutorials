c05b3e2798f1a24c33aa75d887dd105a18d7cde1
Chevischevis <chevischevis@ciencias.unam.mx>
2025-12-28 20:54:08 -0600

    Avance en el uso de cards para la aplicacion de PlacesToVisit

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -6,55 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
+
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.tutorials.ui.theme.TutorialsTheme
-import kotlin.collections.forEachIndexed
-import kotlin.collections.lastIndex
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tutorials.model.PlaceToVisit
 
 
 class MainActivity : ComponentActivity() {
@@ -67,7 +45,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SampleCard()
+                    PlaceCard(
+                        PlaceToVisit(
+                            R.string.place1, R.drawable.image1, R.string.country_mx,
+                            R.string.weather_tempered, R.string.lenguage_es, R.string.description1
+                        )
+                    )
                 }
             }
 
@@ -75,31 +58,79 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun Modifier.sectionPadding(): Modifier =
-    this.padding(horizontal = Dimens.screenPadding, vertical = Dimens.sectionVertical)
-
 @Composable
-fun SampleCard(title: String, subtitle: String) {
-    Card {
-        Column {
-            Text(
-                text = title,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(Dimens.cardPadding)
-            )
-            Text(
-                text = subtitle,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(Dimens.cardPadding)
-            )
+fun PlaceCard(place: PlaceToVisit, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        )
+    ) {
+        Row {
+            Box{
+                Image(
+                    painter = painterResource(place.imageResourceId),
+                    contentDescription = stringResource(place.nameResourceId),
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Row(
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                                .background(color =  Color(240, 191, 31)),
+                    )
+                {
+                    Text(
+                        text = stringResource(place.weatherResourceId),
+                        fontSize = 15.sp,
+                        modifier = Modifier
+                            .padding(start = 10.dp, top = 6.dp, bottom = 6.dp, end = 12.dp)
+                    )
+                }
+                Image(
+                    painter = painterResource(R.drawable.heart),
+                    contentDescription = stringResource(R.string.info),
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
+                        .align(Alignment.BottomEnd)
+                        .padding(10.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(place.nameResourceId),
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+                Text(
+                    text = stringResource(place.countryResourceId),
+                    fontSize = 26.sp,
+                    modifier = modifier.padding(10.dp)
+                )
+                Text(
+                    text = stringResource(place.languageResourceId),
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(10.dp)
+                )
+            }
             Image(
-                painter = painterResource(R.drawable.dragon)
+                painter = painterResource(R.drawable.info),
+                contentDescription = stringResource(R.string.info),
+                modifier = Modifier
+                    .width(40.dp)
+                    .height(40.dp),
+                contentScale = ContentScale.Fit
             )
-
         }
-        Text(text = subtitle)
     }
 }
 
@@ -110,16 +141,38 @@ fun SampleCard(title: String, subtitle: String) {
 @Composable
 fun PreviewMunicipioScreen() {
     MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            MunicipioScreen()
-        }
+        PlaceCard(
+            PlaceToVisit(
+                R.string.place1, R.drawable.image1, R.string.country_mx,
+                R.string.weather_tempered, R.string.lenguage_es, R.string.description1
+            )
+        )
+
     }
 }
 
 @Preview(name = "Font 1.3x", fontScale = 1.3f, showBackground = true)
 @Composable
 fun PreviewFontScale() {
-    TutorialsTheme { MunicipioScreen() }
+    TutorialsTheme {
+        PlaceCard(
+            PlaceToVisit(
+                R.string.place1, R.drawable.image1, R.string.country_mx,
+                R.string.weather_tempered, R.string.lenguage_es, R.string.description1
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    TutorialsTheme {
+        PlaceToVisit(
+            R.string.place1, R.drawable.image1, R.string.country_mx,
+            R.string.weather_tempered, R.string.lenguage_es, R.string.description1
+        )
+    }
 }
 
 @Preview(
@@ -129,7 +182,14 @@ fun PreviewFontScale() {
 )
 @Composable
 fun PreviewDark() {
-    TutorialsTheme { MunicipioScreen() }
+    TutorialsTheme {
+        PlaceCard(
+            PlaceToVisit(
+                R.string.place1, R.drawable.image1, R.string.country_mx,
+                R.string.weather_tempered, R.string.lenguage_es, R.string.description1
+            )
+        )
+    }
 }
 
 
