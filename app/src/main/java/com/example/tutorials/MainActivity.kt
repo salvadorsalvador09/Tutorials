d899a1c8cd66329b283b059a5a34ec910b692756
Chevischevis <chevischevis@ciencias.unam.mx>
2026-01-03 12:08:17 -0600

    Nueva rama para el proyecto de Schedule

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -4,24 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tutorials.ui.theme.TutorialsTheme
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.annotations.Async
 
 
 class MainActivity : ComponentActivity() {
@@ -42,66 +54,171 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageItem(id: Int, description: String, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier.padding(8.dp),
-        elevation = CardDefaults.cardElevation( defaultElevation = 4.dp),
+fun ScheduleCard(number: Int, name: String, time: String, modifier: Modifier = Modifier)
+{
+    OutlinedCard (
+        modifier= Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        border = BorderStroke(1.dp, color = Color.Gray),
         colors = CardDefaults.cardColors(
-            containerColor = Color.White, //Card background color
-            contentColor = Color.DarkGray  //Card content color,e.g.text
+            containerColor = Color.White,
+            contentColor = Color.DarkGray
         )
-    ) {
-        Box{
-            Image(
-                painter =  painterResource(id),
-                contentDescription = description,
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
+    ){
+        Row (
+            modifier = Modifier.padding(10.dp)
+        ){
+            Text(text = number.toString(),
+                fontSize =  100.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(
+                        top = 0.dp,
+                        start = 8.dp,
+                        end = 8.dp,
+                        bottom = 0.dp
+                    )
+                )
+            Column (
+                modifier = Modifier.weight(1f)
+            ){
+                Text(
+                    text = name,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(
+                            top = 16.dp,
+                            start = 8.dp,
+                            end = 8.dp,
+                            bottom = 4.dp
+                        )
+                )
+                Text(
+                    text = time ,
+                     fontSize =  40.sp,
+                    modifier = Modifier
+                        .padding(
+                            top = 4.dp,
+                            start =  8.dp,
+                            end = 8.dp,
+                            bottom = 16.dp
+                        )
+                )
+                Canvas(
+                    modifier = Modifier
+                        .size(16.dp),
+                    onDraw = {
+                        drawCircle(color = Color(0x29, 0x6e, 0xB4))
+                    }
+                )
+            }
         }
     }
 }
 
+
+private const val strings = "Capilla del Rosario"
+
+@Composable
+fun ScheduleList(){
+    val busStops = arrayOf(
+        stringResource(R.string.capilla_del_rosario),
+        stringResource(R.string.catedral_de_puebla),
+        stringResource(R.string.callej_n_de_los_sapos),
+        stringResource(R.string.mercado_el_pari_n),
+        stringResource(R.string.barrio_del_artista),
+        stringResource(R.string.fuertes_de_loreto),
+        stringResource(R.string.convento_secreto_de_sana_m_nica),
+        stringResource(R.string.casa_del_alfe_ique),
+        stringResource(R.string.casa_de_los_mu_ecos),
+        stringResource(R.string.museo_nacional_de_los_ferrocarriles_mexicanos),
+    )
+    val schedules = arrayOf(
+        stringResource(R.string._9_00_am),
+        stringResource(R.string._9_15_am),
+        stringResource(R.string._9_30_am),
+        stringResource(R.string._9_45_am),
+
+        stringResource(R.string._10_00_am),
+        stringResource(R.string._10_15_am),
+        stringResource(R.string._10_30_am),
+        stringResource(R.string._10_45_am),
+
+        stringResource(R.string._11_00_am),
+        stringResource(R.string._11_15_am),
+        stringResource(R.string._11_30_am),
+        stringResource(R.string._11_45_am),
+
+        stringResource(R.string._12_00_pm),
+        stringResource(R.string._12_15_pm),
+        stringResource(R.string._12_30_pm),
+        stringResource(R.string._12_45_pm),
+
+        stringResource(R.string._1_00_pm),
+        stringResource(R.string._1_15_pm),
+        stringResource(R.string._1_30_pm),
+        stringResource(R.string._1_45_pm),
+
+        stringResource(R.string._2_00_pm),
+        stringResource(R.string._2_15_pm),
+        stringResource(R.string._2_30_pm),
+        stringResource(R.string._2_45_pm),
+
+        stringResource(R.string._3_00_pm),
+        stringResource(R.string._3_15_pm),
+        stringResource(R.string._3_30_pm),
+        stringResource(R.string._3_45_pm),
+
+        stringResource(R.string._4_00_pm),
+        stringResource(R.string._4_15_pm),
+        stringResource(R.string._4_30_pm),
+        stringResource(R.string._4_45_pm),
+
+        stringResource(R.string._5_00_pm),
+        stringResource(R.string._5_15_pm),
+        stringResource(R.string._5_30_pm),
+        stringResource(R.string._5_45_pm),
+
+        stringResource(R.string._6_00_pm),
+        stringResource(R.string._6_15_pm),
+        stringResource(R.string._6_30_pm),
+        stringResource(R.string._6_45_pm),
+
+        stringResource(R.string._7_00_pm)
+    )
+
+    LazyColumn {
+        items(schedules.size) { index ->
+            ScheduleCard(index + 1, busStops[index%busStops.size],
+                schedules[index])
+        }
+    }
+
+}
 
 
 
 @Composable
 private fun StraggeredGrid()
 {
-    val images= intArrayOf(R.drawable.butterfly, R.drawable.cat, R.drawable.crane,
-        R.drawable.dog, R.drawable.elephant, R.drawable.fish, R.drawable.fox,
-        R.drawable.fox, R.drawable.frog, R.drawable.horse, R.drawable.owl,
-        R.drawable.owl, R.drawable.rabbit, R.drawable.whale, R.drawable.butterfly,
-        R.drawable.fox, R.drawable.frog, R.drawable.horse
-        )
-    val description = arrayOf(
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-        stringResource(R.string.butterfly), stringResource(R.string.cat),
-    )
+
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Fixed(3),
         verticalItemSpacing = 4.dp
     ) {
-        items(images.size){
-            index-> ImageItem(images[index], description[index])
-        }
+
     }
 }
 
 
 
 
-@Preview
+@Preview(showBackground =  true)
 @Composable
-fun StageredgridPreviewPreview() {
+fun SchedulePreview() {
     TutorialsTheme {
-        StraggeredGrid()
+        ScheduleList()
     }
 }
 
